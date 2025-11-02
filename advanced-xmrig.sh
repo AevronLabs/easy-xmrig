7cfddd36e50af74a6d9033aa22187b20545ed7ef
BukkitEventListener <103391432+QXIoa@users.noreply.github.com>
2025-11-02 09:02:53 -0500

    Update advanced-xmrig.sh to remove existing xmrig directory

--- advanced-xmrig.sh
+++ advanced-xmrig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 startdir=$(pwd)
 sudo apt-get install git build-essential cmake automake libtool autoconf -y
+[ -d xmrig ] && rm -rf xmrig > /dev/null 2>&1
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/scripts
 ./build_deps.sh && cd ../build
