239ee00fb7156b92a842b8d113c6b64199f73d4e
BukkitEventListener <103391432+QXIoa@users.noreply.github.com>
2025-11-16 14:12:00 -0500

    Modify xmrig clone command based on USE_DEFAULT
    Add conditional cloning of xmrig repository based on USE_DEFAULT variable.

--- advanced-xmrig.sh
+++ advanced-xmrig.sh
@@ -2,7 +2,11 @@
 startdir=$(pwd)
 sudo apt-get install git build-essential cmake automake libtool autoconf -y
 [ -d xmrig ] && rm -rf xmrig > /dev/null 2>&1
-git clone https://github.com/xmrig/xmrig.git
+if [ "$USE_DEFAULT" = "1" ]; then
+    git clone https://github.com/xmrig/xmrig.git
+else
+    git clone https://github.com/AevronLabs/xmrig.git
+fi
 mkdir xmrig/build && cd xmrig/scripts
 ./build_deps.sh && cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps
